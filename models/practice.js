799e0e65ebc937b1f8241b7211e08b853190c5ae
Br1 <brunogiovagnoli@gmail.com>
2018-09-22 07:26:22 -0300

    add practice model again, add practice views

---
+++ models/practice.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+let practiceSchema = new mongoose.Schema({
+    type: String, // offer or request
+    entity: String, // school name or company name
+    description: String,
+    level: String, // offer: superior/tecnico
+    date: String, // when the practice was created
+    phone: String, // private
+    email: String // private
+});
+
+let practiceModel = mongoose.model('Practice', practiceSchema);
+
+module.exports = practiceModel;
